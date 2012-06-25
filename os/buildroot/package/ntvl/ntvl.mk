efb1459b7876832460e6440fcb226c595a316f33
Mario Ricardo Rodriguez Somohano <collab@bambusoft.com>
2012-06-25 15:59:59 -0500

    Updated version references an added installation of ntvl.conf

--- os/buildroot/package/ntvl/ntvl.mk
+++ os/buildroot/package/ntvl/ntvl.mk
@@ -3,19 +3,21 @@
 # ntvl
 #
 #############################################################
-NTVL_VERSION = 1.0.0-RC
+NTVL_VERSION = 1.0.0
 NTVL_SOURCE = bambusoft-ntvl-$(NTVL_VERSION).tar.gz
 NTVL_SITE = http://ntvl.bambusoft.mx/downloads
 
 define NTVL_BUILD_CMDS
-	$(SED) 's/########/OPTIONS=-static/' $(@D)/Makefile
+	$(SED) 's/OPTIONS=/OPTIONS=-static/' $(@D)/Makefile
 	$(MAKE1) -C $(@D)
 endef
 
 define NTVL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/edge $(TARGET_DIR)/sbin/edge
-	$(INSTALL) -D -m 0755 $(@D)/supernode $(TARGET_DIR)/sbin/supernode
-	$(INSTALL) -D -m 0755 $(@D)/tunnel $(TARGET_DIR)/sbin/tunnel
+	$(INSTALL) -D -m 0755 $(@D)/config/ntvl-node-default.conf $(TARGET_DIR)/etc/ntvl/ntvl.conf
+	$(INSTALL) -D -m 0755 $(@D)/dist/ntvld $(TARGET_DIR)/sbin/ntvld
+	$(INSTALL) -D -m 0755 $(@D)/dist/node $(TARGET_DIR)/sbin/node
+	$(INSTALL) -D -m 0755 $(@D)/dist/supernode $(TARGET_DIR)/sbin/supernode
+	$(INSTALL) -D -m 0755 $(@D)/dist/tunnel $(TARGET_DIR)/sbin/tunnel
 endef
 
 $(eval $(call GENTARGETS))
