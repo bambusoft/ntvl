edff0b7d27682eb3913d183583c102b2d0d29717
Mario Ricardo Rodriguez Somohano <collab@bambusoft.com>
2012-06-25 17:40:35 -0500

    Install config file revised

--- os/buildroot/package/ntvl/ntvl.mk
+++ os/buildroot/package/ntvl/ntvl.mk
@@ -13,9 +13,9 @@ define NTVL_BUILD_CMDS
 endef
 
 define NTVL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/config/ntvl-node-default.conf $(TARGET_DIR)/etc/ntvl/ntvl.conf
+	$(INSTALL) -D -m 0644 $(@D)/config/ntvl-node-default.conf $(TARGET_DIR)/etc/ntvl/ntvl.conf
 	$(INSTALL) -D -m 0755 $(@D)/dist/ntvld $(TARGET_DIR)/sbin/ntvld
-	$(INSTALL) -D -m 0755 $(@D)/dist/node $(TARGET_DIR)/sbin/node
+	$(INSTALL) -D -m 0755 $(@D)/dist/node $(TARGET_DIR)/sbin/edge
 	$(INSTALL) -D -m 0755 $(@D)/dist/supernode $(TARGET_DIR)/sbin/supernode
 	$(INSTALL) -D -m 0755 $(@D)/dist/tunnel $(TARGET_DIR)/sbin/tunnel
 endef
